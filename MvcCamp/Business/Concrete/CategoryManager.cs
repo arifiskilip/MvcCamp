eb9bca57174f98f078602b04ad599d84e4b4c19f
Arif İskilip <71379630+arifiskilip@users.noreply.github.com>
2022-03-27 00:01:58 +0300

    Dictionary
    Homework 1

---
+++ MvcCamp/Business/Concrete/CategoryManager.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager
+    {
+        ICategoryDal _categoryDal;
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public void Add(Category category)
+        {
+            if (category.Name=="" || category.Name.Length<=3 || category.Description=="")
+            {
+                //
+            }
+            else
+            {
+                _categoryDal.Add(category);
+            }
+        }
+    }
+}
